6b8b928fe625234537fb45ef9a3989b2a62cbb5d
Nirex0 <Nirex.0@gmail.com>
2018-11-24 22:40:17 +0330

    Update: NPattern now supports Text and Byte processing

--- src/NES/NPatern.cpp
+++ src/NES/NPatern.cpp
@@ -27,27 +27,27 @@ std::string NPatern::Text_Perfom(std::string text, std::string key)
 	std::string intext = text;
 	for (auto& p : m_paterns)
 	{
-		if (p == "A0")
+		if (p == "TA0")
 		{
 			intext = NES::TEXT_NESA0(intext, key);
 		}
-		else if (p == "A1")
+		else if (p == "TA1")
 		{
 			intext = NES::TEXT_NESA1(intext, key);
 		}
-		else if (p == "B0")
+		else if (p == "TB0")
 		{
 			intext = NES::TEXT_NESB0(intext, key);
 		}
-		else if (p == "B1")
+		else if (p == "TB1")
 		{
 			intext = NES::TEXT_NESB1(intext, key);
 		}
-		else if (p == "C0")
+		else if (p == "TC0")
 		{
 			intext = NES::TEXT_NESC0(intext, key);
 		}
-		else if (p == "C1")
+		else if (p == "TC1")
 		{
 			intext = NES::TEXT_NESC1(intext, key);
 		}
@@ -57,5 +57,33 @@ std::string NPatern::Text_Perfom(std::string text, std::string key)
 
 std::vector<char> NPatern::Data_Perform(std::vector<char> data, std::string key)
 {
-	return std::vector<char>();
+	std::vector<char> indata;
+	for (auto& p : m_paterns)
+	{
+		if (p == "DA0")
+		{
+			indata = NES::DATA_NESA0(indata, key);
+		}
+		else if (p == "DA1")
+		{
+			indata = NES::DATA_NESA1(indata, key);
+		}
+		else if (p == "DB0")
+		{
+			indata = NES::DATA_NESB0(indata, key);
+		}
+		else if (p == "DB1")
+		{
+			indata = NES::DATA_NESB1(indata, key);
+		}
+		else if (p == "DC0")
+		{
+			indata = NES::DATA_NESC0(indata, key);
+		}
+		else if (p == "DC1")
+		{
+			indata = NES::DATA_NESC1(indata, key);
+		}
+	}
+	return indata;
 }
