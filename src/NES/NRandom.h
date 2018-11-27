cd4778ddd45f471b6ec29435369d6776e4ee791c
Nirex0 <Nirex.0@gmail.com>
2018-11-28 01:47:11 +0330

    Update: NRandom Now header-only

--- src/NES/NRandom.h
+++ src/NES/NRandom.h
@@ -3,10 +3,23 @@
 #ifndef _N_RANDOM_H_
 #define _N_RANDOM_H_
 
+#include <random>
+
 class NRandom
 {
 public:
-	int GetNumber(int min, int max);
+	static inline int GetNumber(int min, int max, int seed)
+	{
+		std::uniform_int_distribution<std::mt19937::result_type> udist(min, max);
+		std::mt19937 rng;
+
+		std::mt19937::result_type const seedval = seed;
+		rng.seed(seedval);
+
+		std::mt19937::result_type random_number = udist(rng);
+
+		return random_number;
+	}
 };
 
 #endif // !_N_RANDOM_H_
