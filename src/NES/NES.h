b23ed453078430160040a316c20915729ca76834
Nirex0 <Nirex.0@gmail.com>
2018-11-28 00:04:51 +0330

    Remove: File Support forward decls

--- src/NES/NES.h
+++ src/NES/NES.h
@@ -17,16 +17,6 @@ namespace NES
 																  
 	std::string TEXT_NESC0(std::string text, std::string key); // TC0
 	std::string TEXT_NESC1(std::string text, std::string key); // TC1
-
-	std::vector<char> DATA_NESA0(std::vector<char> data, std::string key); // DA0
-	std::vector<char> DATA_NESA1(std::vector<char> data, std::string key); // DA1
-					  														  
-	std::vector<char> DATA_NESB0(std::vector<char> data, std::string key); // DB0
-	std::vector<char> DATA_NESB1(std::vector<char> data, std::string key); // DB1
-		 			  			 											  
-	std::vector<char> DATA_NESC0(std::vector<char> data, std::string key); // DC0
-	std::vector<char> DATA_NESC1(std::vector<char> data, std::string key); // DC1
-
 }
 
 #endif // !_N_NES_H_
