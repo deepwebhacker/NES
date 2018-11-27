d72d9bf61c387545174bd5d99d7d2b91ae00f4cf
Nirex0 <Nirex.0@gmail.com>
2018-11-28 01:45:42 +0330

    Update NES.cpp
    Add: Time to A and B
    Add: Random to A and B
    
    Fix: Warnings
    Fix: Bugs with B

--- src/NES/NES.cpp
+++ src/NES/NES.cpp
@@ -1,18 +1,41 @@
 //© 2018 NIREX ALL RIGHTS RESERVED
 
 #include "NES.h"
+#include "NTime.h"
+#include "NRandom.h"
 
 namespace NES
 {
 	std::string TEXT_NESA0(std::string text, std::string key)
 	{
+		std::vector<unsigned char> time = NTime::DataFromEpoch(NTime::GrabEpoch());
+
+		 time[0] *= NRandom::GetNumber(100, 200, 1);
+		 time[1] *= NRandom::GetNumber(200, 300, 2);
+		 time[2] *= NRandom::GetNumber(300, 400, 3);
+		 time[3] *= NRandom::GetNumber(400, 500, 4);
+		
 		std::vector<unsigned char> vec;
-		for (int i = 0; i < text.length(); i++)
+		int TimeIterator = 0;
+
+		for (int i = 0; i < (int)time.size(); i++)
+		{
+			vec.push_back(time[i]);
+		}
+
+		for (int i = 0; i < (int)text.length(); i++)
 		{
 			char l;
-			for (int j = 0; j < key.length(); j++)
+			unsigned char TimeKey = time[TimeIterator];
+			for (int j = 0; j < (int)key.length(); j++)
 			{
-				l = static_cast<unsigned char>(text[i] + (2 * key[j] * pow(key[j], 2)));
+				l = static_cast<unsigned char>(text[i] + (2 * key[j] * pow(key[j], 2) + TimeKey));
+		
+			}
+			TimeIterator++;
+			if (TimeIterator == time.size())
+			{
+				TimeIterator = 0;
 			}
 			vec.push_back(l);
 		}
@@ -27,17 +50,32 @@ namespace NES
 	std::string TEXT_NESA1(std::string text, std::string key)
 	{
 		std::vector<unsigned char> vec;
-		for (int i = 0; i < text.length(); i++)
+		std::vector<unsigned char> time;
+
+		int TimeIterator = 0;
+
+		time.push_back(text[0]);
+		time.push_back(text[1]);
+		time.push_back(text[2]);
+		time.push_back(text[3]);
+
+		for (int i = 4; i < (int)text.length(); i++)
 		{
 			char l;
-			for (int j = 0; j <  key.length(); j++)
+			unsigned char TimeKey = time[TimeIterator];
+			for (int j = 0; j < (int)key.length(); j++)
 			{
-				l = static_cast<unsigned char>(text[i] - (2 * key[j] * pow(key[j], 2)));
+				l = static_cast<unsigned char>(text[i] - (2 * key[j] * pow(key[j], 2) + TimeKey));
+			}
+			TimeIterator++;
+			if (TimeIterator == time.size())
+			{
+				TimeIterator = 0;
 			}
 			vec.push_back(l);
 		}
 		std::string returnStr = "";
-		for (int i = 0; i < vec.size(); i++)
+		for (int i = 0; i < (int)vec.size(); i++)
 		{
 			returnStr += (char)vec[i];
 		}
@@ -46,14 +84,27 @@ namespace NES
 				
 	std::string TEXT_NESB0(std::string text, std::string key)
 	{
+		std::vector<unsigned char> time = NTime::DataFromEpoch(NTime::GrabEpoch());
+
+		time[0] *= NRandom::GetNumber(170, 240, 5);
+		time[1] *= NRandom::GetNumber(270, 340, 6);
+		time[2] *= NRandom::GetNumber(370, 440, 7);
+		time[3] *= NRandom::GetNumber(470, 540, 8);
+
 		std::vector<unsigned char> data;
-		unsigned long long keyX = 0;
-		for (int i = 0; i < key.length(); i++)
+		data.push_back(time[0]);
+		data.push_back(time[1]);
+		data.push_back(time[2]);
+		data.push_back(time[3]);
+
+		unsigned char keyX = 0;
+		for (int i = 0; i < (int)key.length(); i++)
 		{
 			keyX += (unsigned char)key[i];
 		}
+		keyX += time[0] + time[1] + time[2] + time[3];
 
-		for (int i = 0; i < text.length(); i++)
+		for (int i = 0; i < (int)text.length(); i++)
 		{
 			unsigned long olmessage = (unsigned char)text[i] * keyX;
 			unsigned long numerator = olmessage / 255;
@@ -64,7 +115,7 @@ namespace NES
 			unsigned char b3 = ((numerator >> 16) & 0xFF);
 			unsigned char b4 = ((numerator >> 24) & 0xFF);
 
-			data.push_back(remainder);
+			data.push_back((unsigned char)remainder);
 			data.push_back((unsigned char)b1);
 			data.push_back((unsigned char)b2);
 			data.push_back((unsigned char)b3);
@@ -72,7 +123,6 @@ namespace NES
 		}
 
 		std::string returnStr = "";
-		
 		for (int i = 0; i < (int)data.size(); i++)
 		{
 			returnStr += (char)data[i];
@@ -83,18 +133,14 @@ namespace NES
 	std::string TEXT_NESB1(std::string text, std::string key)
 	{
 		std::vector<unsigned char> data;
-		unsigned long long keyX = 0;
-		for (int i = 0; i < key.length(); i++)
+		unsigned char keyX = 0;
+		for (int i = 0; i < (int)key.length(); i++)
 		{
 			keyX += (unsigned char)key[i];
 		}
+		keyX += text[0] + text[1] + text[2] + text[3];
 
-		while (text.length() % 5 != 0)
-		{
-			text += " ";
-		}
-
-		for (int i = 0; i < text.length(); i += 5)
+		for (int i = 4; i < (int)text.length(); i += 5)
 		{
 			unsigned long remainder = (unsigned char)text[i];
 			unsigned long numerator
@@ -121,17 +167,17 @@ namespace NES
 	std::string TEXT_NESC0(std::string text, std::string key)
 	{
 		std::vector<char> vec;
-		for (int i = 0; i <  text.length(); i++)
+		for (int i = 0; i <  (int)text.length(); i++)
 		{
 			char l;
-			for (int j = 0; j < key.length(); j++)
+			for (int j = 0; j < (int)key.length(); j++)
 			{
 				l = static_cast<char>(text[i] + (key[j]));
 			}
 			vec.push_back(l);
 		}
 		std::string returnStr = "";
-		int size = static_cast<int>(vec.size());
+		int size = static_cast<int>((int)vec.size());
 		for (int i = 0; i < size; i++)
 		{
 			returnStr += vec[i];
@@ -142,10 +188,10 @@ namespace NES
 	std::string TEXT_NESC1(std::string text, std::string key)
 	{
 		std::vector<char> vec;
-		for (int i = 0; i < text.length(); i++)
+		for (int i = 0; i < (int)text.length(); i++)
 		{
 			char l;
-			for (int j = 0; j < key.length(); j++)
+			for (int j = 0; j <(int)key.length(); j++)
 			{
 				l = static_cast<char>(text[i] - (key[j]));
 			}
