ccc04c39daa5db6fb90e7f8cf1bcdd7d7cf5b37b
Nirex0 <Nirex.0@gmail.com>
2018-11-28 00:01:21 +0330

    Update: B is now Implemented with an entirely new formula (Much Stronger than before)

--- src/NES/NES.cpp
+++ src/NES/NES.cpp
@@ -6,84 +6,114 @@ namespace NES
 {
 	std::string TEXT_NESA0(std::string text, std::string key)
 	{
-		std::vector<char> vec;
+		std::vector<unsigned char> vec;
 		for (int i = 0; i < text.length(); i++)
 		{
 			char l;
 			for (int j = 0; j < key.length(); j++)
 			{
-				l = static_cast<char>(text[i] + (2 * key[j] * pow(key[j], 2) + (8 * key[j])));
+				l = static_cast<unsigned char>(text[i] + (2 * key[j] * pow(key[j], 2)));
 			}
 			vec.push_back(l);
 		}
 		std::string returnStr = "";
-		int size = static_cast<int>(vec.size());
-		for (int i = 0; i < size; i++)
+		for (int i = 0; i < (int)vec.size(); i++)
 		{
-			returnStr += vec[i];
+			returnStr += (char)vec[i];
 		}
 		return returnStr;
 	}
 				
 	std::string TEXT_NESA1(std::string text, std::string key)
 	{
-		std::vector<char> vec;
+		std::vector<unsigned char> vec;
 		for (int i = 0; i < text.length(); i++)
 		{
 			char l;
 			for (int j = 0; j <  key.length(); j++)
 			{
-				l = static_cast<char>(text[i] - (2 * key[j] * pow(key[j], 2) + (8 * key[j])));
+				l = static_cast<unsigned char>(text[i] - (2 * key[j] * pow(key[j], 2)));
 			}
 			vec.push_back(l);
 		}
 		std::string returnStr = "";
-		int size = static_cast<int>(vec.size());
-		for (int i = 0; i < size; i++)
+		for (int i = 0; i < vec.size(); i++)
 		{
-			returnStr += vec[i];
+			returnStr += (char)vec[i];
 		}
 		return returnStr;
 	}
 				
 	std::string TEXT_NESB0(std::string text, std::string key)
 	{
-		std::vector<char> vec;
+		std::vector<unsigned char> data;
+		unsigned long long keyX = 0;
+		for (int i = 0; i < key.length(); i++)
+		{
+			keyX += (unsigned char)key[i];
+		}
+
 		for (int i = 0; i < text.length(); i++)
 		{
-			char l;
-			for (int j = 0; j < key.length(); j++)
-			{
-				l = static_cast<char>(text[i] - ((2 * key[j]) + (key[j] / 2)));
-			}
-			vec.push_back(l);
+			unsigned long olmessage = (unsigned char)text[i] * keyX;
+			unsigned long numerator = olmessage / 255;
+			unsigned long remainder = olmessage % 255;
+
+			unsigned char b1 = (numerator & 0xFF);
+			unsigned char b2 = ((numerator >> 8) & 0xFF);
+			unsigned char b3 = ((numerator >> 16) & 0xFF);
+			unsigned char b4 = ((numerator >> 24) & 0xFF);
+
+			data.push_back(remainder);
+			data.push_back((unsigned char)b1);
+			data.push_back((unsigned char)b2);
+			data.push_back((unsigned char)b3);
+			data.push_back((unsigned char)b4);
 		}
+
 		std::string returnStr = "";
-		int size = static_cast<int>(vec.size());
-		for (int i = 0; i < size; i++)
+		
+		for (int i = 0; i < (int)data.size(); i++)
 		{
-			returnStr += vec[i];
+			returnStr += (char)data[i];
 		}
 		return returnStr;
 	}
 				
 	std::string TEXT_NESB1(std::string text, std::string key)
 	{
-		std::vector<char> vec;
-		for (int i = 0; i < text.length(); i++)
+		std::vector<unsigned char> data;
+		unsigned long long keyX = 0;
+		for (int i = 0; i < key.length(); i++)
 		{
-			char l;
-			for (int j = 0; j < key.length(); j++)
-			{
-				l = static_cast<char>(text[i] + ((2 * key[j]) + (key[j] / 2)));
-			}
-			vec.push_back(l);
+			keyX += (unsigned char)key[i];
 		}
-		std::string returnStr = "";
-		int size = static_cast<int>(vec.size());
-		for (int i = 0; i < size; i++)
+
+		while (text.length() % 5 != 0)
 		{
-			returnStr += vec[i];
+			text += " ";
+		}
+
+		for (int i = 0; i < text.length(); i += 5)
+		{
+			unsigned long remainder = (unsigned char)text[i];
+			unsigned long numerator
+				= ((unsigned long)(((unsigned char)text[i + 4]) << 24)
+					| (unsigned long)(((unsigned char)text[i + 3]) << 16)
+					| (unsigned long)(((unsigned char)text[i + 2]) << 8)
+					| (unsigned long)((unsigned char)text[i + 1]));
+
+
+			unsigned long olmessage = (numerator * 255) + remainder;
+			unsigned char letter = (unsigned char)(olmessage / keyX);
+			data.push_back(letter);
+		}
+
+		std::string returnStr = "";
+
+		for (int i = 0; i < (int)data.size(); i++)
+		{
+			returnStr += (char)data[i];
 		}
 		return returnStr;
 	}
