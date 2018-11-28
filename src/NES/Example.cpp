537e27edf1b6d661a249a1fc70369495293fd143
Nirex0 <Nirex.0@gmail.com>
2018-11-28 17:27:49 +0330

    Update: Examples files

--- src/NES/Example.cpp
+++ src/NES/Example.cpp
@@ -43,6 +43,9 @@ FILL main(INT argc, NStrArray argv) -> INT
 	szText = "The Secret Message!";
 	szKey = "The Master Key is: \"Master Secret Key!!!!XXXXXXXXXXXX\"";
 
+	//szText = "T";
+	//szKey = "The Master Key is: \"Master Secret Key!!!!XXXXXXXXXXXX\"";
+
 	COUT << ENDL;
 
 	szEncrypted = NES::TEXT_NESA0(szText, szKey);
@@ -52,7 +55,6 @@ FILL main(INT argc, NStrArray argv) -> INT
 	szEncrypted02 = NES::TEXT_NESA0(szText, szKey);
 	Sleep(10);
 	szEncrypted03 = NES::TEXT_NESA0(szText, szKey);
-	Sleep(10);
 	szDecrypted = NES::TEXT_NESA1(szEncrypted, szKey);
 	szDecrypted01 = NES::TEXT_NESA1(szEncrypted01, szKey);
 	szDecrypted02 = NES::TEXT_NESA1(szEncrypted02, szKey);
@@ -87,7 +89,6 @@ FILL main(INT argc, NStrArray argv) -> INT
 	szEncrypted02 = NES::TEXT_NESB0(szText, szKey);
 	Sleep(10);
 	szEncrypted03 = NES::TEXT_NESB0(szText, szKey);
-	Sleep(10);
 	szDecrypted = NES::TEXT_NESB1(szEncrypted, szKey);
 	szDecrypted01 = NES::TEXT_NESB1(szEncrypted01, szKey);
 	szDecrypted02 = NES::TEXT_NESB1(szEncrypted02, szKey);
@@ -114,7 +115,41 @@ FILL main(INT argc, NStrArray argv) -> INT
 	COUT << "Decrypted(B_03): ";
 	COUT << szDecrypted03 << ENDL;
 	COUT << ENDL;
-							   
+
+
+	szEncrypted = NES::TEXT_NESC0(szText, szKey);
+	Sleep(10);
+	szEncrypted01 = NES::TEXT_NESC0(szText, szKey);
+	Sleep(10);
+	szEncrypted02 = NES::TEXT_NESC0(szText, szKey);
+	Sleep(10);
+	szEncrypted03 = NES::TEXT_NESC0(szText, szKey);
+	szDecrypted = NES::TEXT_NESC1(szEncrypted, szKey);
+	szDecrypted01 = NES::TEXT_NESC1(szEncrypted01, szKey);
+	szDecrypted02 = NES::TEXT_NESC1(szEncrypted02, szKey);
+	szDecrypted03 = NES::TEXT_NESC1(szEncrypted03, szKey);
+
+	COUT << "Encrypted(C_00): ";
+	COUT << szEncrypted << ENDL;
+	COUT << "Encrypted(C_01): ";
+	COUT << szEncrypted01 << ENDL;
+	COUT << "Encrypted(C_02): ";
+	COUT << szEncrypted02 << ENDL;
+	COUT << "Encrypted(C_03): ";
+	COUT << szEncrypted03 << ENDL;
+
+	COUT << ENDL;
+
+	COUT << "Decrypted(C_00): ";
+	COUT << szDecrypted << ENDL;
+	COUT << "Decrypted(C_01): ";
+	COUT << szDecrypted01 << ENDL;
+	COUT << "Decrypted(C_02): ";
+	COUT << szDecrypted02 << ENDL;
+	COUT << "Decrypted(C_03): ";
+	COUT << szDecrypted03 << ENDL;
+	COUT << ENDL;
+
 
 	int x;
 	CIN >> x;
