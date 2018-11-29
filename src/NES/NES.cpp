242a123c8f9eb37201bb6bcd9e8fe7aa5f0087e5
Nirex0 <Nirex.0@gmail.com>
2018-11-29 11:55:30 +0330

    Add: File Support to NES

--- src/NES/NES.cpp
+++ src/NES/NES.cpp
@@ -6,7 +6,7 @@
 
 namespace NES
 {
-	std::string TEXT_NESA0(std::string text, std::string key)
+	std::string NESA0(std::string text, std::string key)
 	{
 		std::vector<unsigned char> time = NTime::DataFromEpoch(NTime::GrabEpoch());
 
@@ -47,7 +47,7 @@ namespace NES
 		return returnStr;
 	}
 				
-	std::string TEXT_NESA1(std::string text, std::string key)
+	std::string NESA1(std::string text, std::string key)
 	{
 		std::vector<unsigned char> vec;
 		std::vector<unsigned char> time;
@@ -82,7 +82,7 @@ namespace NES
 		return returnStr;
 	}
 
-	std::string TEXT_NESB0(std::string text, std::string key)
+	std::string NESB0(std::string text, std::string key)
 	{
 		std::vector<unsigned char> time = NTime::DataFromEpoch(NTime::GrabEpoch());
 
@@ -130,7 +130,7 @@ namespace NES
 		return returnStr;
 	}
 				
-	std::string TEXT_NESB1(std::string text, std::string key)
+	std::string NESB1(std::string text, std::string key)
 	{
 		std::vector<unsigned char> data;
 		unsigned char keyX = 0;
@@ -164,7 +164,7 @@ namespace NES
 		return returnStr;
 	}
 				
-	std::string TEXT_NESC0(std::string text, std::string key)
+	std::string NESC0(std::string text, std::string key)
 	{
 		std::vector<unsigned char> time = NTime::DataFromEpoch(NTime::GrabEpoch());
 
@@ -218,7 +218,7 @@ namespace NES
 		return returnStr;
 	}
 				
-	std::string TEXT_NESC1(std::string text, std::string key)
+	std::string NESC1(std::string text, std::string key)
 	{
 		std::vector<unsigned char> data;
 		std::vector<unsigned char> vecRem;
@@ -270,4 +270,239 @@ namespace NES
 		}
 		return returnStr;
 	}
+
+	std::vector<BYTE> NESA0(std::vector<BYTE> data, std::string key)
+	{
+		std::vector<unsigned char> time = NTime::DataFromEpoch(NTime::GrabEpoch());
+
+		time[0] *= NRandom::GetNumber(100, 200, 1);
+		time[1] *= NRandom::GetNumber(200, 300, 2);
+		time[2] *= NRandom::GetNumber(300, 400, 3);
+		time[3] *= NRandom::GetNumber(400, 500, 4);
+
+		std::vector<unsigned char> vec;
+		int TimeIterator = 0;
+
+		for (int i = 0; i < (int)time.size(); i++)
+		{
+			vec.push_back(time[i]);
+		}
+
+		for (int i = 0; i < (int)data.size(); i++)
+		{
+			char l;
+			unsigned char TimeKey = time[TimeIterator];
+			for (int j = 0; j < (int)key.length(); j++)
+			{
+				l = static_cast<unsigned char>(data[i] + (2 * key[j] * pow(key[j], 2) + TimeKey));
+
+			}
+			TimeIterator++;
+			if (TimeIterator == time.size())
+			{
+				TimeIterator = 0;
+			}
+			vec.push_back(l);
+		}
+		return vec;
+	}
+
+	std::vector<BYTE> NESA1(std::vector<BYTE> data, std::string key)
+	{
+		std::vector<unsigned char> vec;
+		std::vector<unsigned char> time;
+
+		int TimeIterator = 0;
+
+		time.push_back(data[0]);
+		time.push_back(data[1]);
+		time.push_back(data[2]);
+		time.push_back(data[3]);
+
+		for (int i = 4; i < (int)data.size(); i++)
+		{
+			char l;
+			unsigned char TimeKey = time[TimeIterator];
+			for (int j = 0; j < (int)key.length(); j++)
+			{
+				l = static_cast<unsigned char>(data[i] - (2 * key[j] * pow(key[j], 2) + TimeKey));
+			}
+			TimeIterator++;
+			if (TimeIterator == time.size())
+			{
+				TimeIterator = 0;
+			}
+			vec.push_back(l);
+		}
+		return vec;
+	}
+
+	std::vector<BYTE> NESB0(std::vector<BYTE> data, std::string key)
+	{
+		std::vector<unsigned char> time = NTime::DataFromEpoch(NTime::GrabEpoch());
+
+		time[0] *= NRandom::GetNumber(170, 240, 5);
+		time[1] *= NRandom::GetNumber(270, 340, 6);
+		time[2] *= NRandom::GetNumber(370, 440, 7);
+		time[3] *= NRandom::GetNumber(470, 540, 8);
+
+		std::vector<unsigned char> vec;
+		vec.push_back(time[0]);
+		vec.push_back(time[1]);
+		vec.push_back(time[2]);
+		vec.push_back(time[3]);
+
+		unsigned char keyX = 0;
+		for (int i = 0; i < (int)key.length(); i++)
+		{
+			keyX += (unsigned char)key[i];
+		}
+		keyX += time[0] + time[1] + time[2] + time[3];
+
+		for (int i = 0; i < (int)data.size(); i++)
+		{
+			unsigned long olmessage = (unsigned char)data[i] * keyX;
+			unsigned long numerator = olmessage / 255;
+			unsigned long remainder = olmessage % 255;
+
+			unsigned char b1 = (numerator & 0xFF);
+			unsigned char b2 = ((numerator >> 8) & 0xFF);
+			unsigned char b3 = ((numerator >> 16) & 0xFF);
+			unsigned char b4 = ((numerator >> 24) & 0xFF);
+
+			vec.push_back((unsigned char)remainder);
+			vec.push_back((unsigned char)b1);
+			vec.push_back((unsigned char)b2);
+			vec.push_back((unsigned char)b3);
+			vec.push_back((unsigned char)b4);
+		}
+
+		return vec;
+	}
+
+	std::vector<BYTE> NESB1(std::vector<BYTE> data, std::string key)
+	{
+		std::vector<unsigned char> vec;
+		unsigned char keyX = 0;
+		for (int i = 0; i < (int)key.length(); i++)
+		{
+			keyX += (unsigned char)key[i];
+		}
+		keyX += data[0] + data[1] + data[2] + data[3];
+
+		for (int i = 4; i < (int)data.size(); i += 5)
+		{
+			unsigned long remainder = (unsigned char)data[i];
+			unsigned long numerator
+				= ((unsigned long)(((unsigned char)data[i + 4]) << 24)
+					| (unsigned long)(((unsigned char)data[i + 3]) << 16)
+					| (unsigned long)(((unsigned char)data[i + 2]) << 8)
+					| (unsigned long)((unsigned char)data[i + 1]));
+
+			unsigned long olmessage = (numerator * 255) + remainder;
+			unsigned char letter = (unsigned char)(olmessage / keyX);
+			vec.push_back(letter);
+		}
+
+		return vec;
+	}
+
+	std::vector<BYTE> NESC0(std::vector<BYTE> data, std::string key)
+	{
+		std::vector<unsigned char> time = NTime::DataFromEpoch(NTime::GrabEpoch());
+
+		time[0] *= NRandom::GetNumber(170, 240, 5);
+		time[1] *= NRandom::GetNumber(270, 340, 6);
+		time[2] *= NRandom::GetNumber(370, 440, 7);
+		time[3] *= NRandom::GetNumber(470, 540, 8);
+
+		std::vector<unsigned char> vec;
+		vec.push_back(time[0]);
+		vec.push_back(time[1]);
+		vec.push_back(time[2]);
+		vec.push_back(time[3]);
+
+		unsigned char KeyX = 0;
+		for (size_t i = 0; i < key.length(); i++)
+		{
+			KeyX += key[i];
+		}
+		KeyX += time[0] + time[1] + time[2] + time[3];
+		KeyX /= key.length();
+		float SqKeyX = sqrt(KeyX);
+
+		for (int i = 0; i < (int)data.size(); i++)
+		{
+			float SqText = sqrt(data[i]);
+			float mainC = (SqText + SqKeyX);
+			mainC *= 1000000;
+			float mainCX = sqrt(mainC);
+			unsigned long long mainCNormalized = (unsigned long long)(mainCX * 1000000);
+
+			unsigned char remainder = mainCNormalized % 255;
+			unsigned long numerator = mainCNormalized / 255;
+
+			unsigned char b1 = (numerator & 0xFF);
+			unsigned char b2 = ((numerator >> 8) & 0xFF);
+			unsigned char b3 = ((numerator >> 16) & 0xFF);
+			unsigned char b4 = ((numerator >> 24) & 0xFF);
+
+			vec.push_back(remainder);
+			vec.push_back(b1);
+			vec.push_back(b2);
+			vec.push_back(b3);
+			vec.push_back(b4);
+		}
+		
+		return vec;
+	}
+
+	std::vector<BYTE> NESC1(std::vector<BYTE> data, std::string key)
+	{
+		std::vector<unsigned char> vec;
+		std::vector<unsigned char> vecRem;
+		std::vector<unsigned long> vecNum;
+
+		unsigned char KeyX = 0;
+		for (size_t i = 0; i < key.length(); i++)
+		{
+			KeyX += key[i];
+		}
+		KeyX += data[0] + data[1] + data[2] + data[3];
+		KeyX /= key.length();
+		float SqKeyX = sqrt(KeyX);
+
+		for (size_t i = 4; i < data.size(); i += 5)
+		{
+			unsigned char remainder = (unsigned char)data[i];
+
+			unsigned long numerator =
+				((unsigned long)(((unsigned char)data[i + 4]) << 24)
+					| (unsigned long)(((unsigned char)data[i + 3]) << 16)
+					| (unsigned long)(((unsigned char)data[i + 2]) << 8)
+					| (unsigned long)((unsigned char)data[i + 1]));
+
+			vecRem.push_back(remainder);
+			vecNum.push_back(numerator);
+		}
+
+		for (size_t i = 0; i < vecRem.size(); i++)
+		{
+			float currentOperand = ((vecNum[i] * 255) + vecRem[i]);
+			unsigned long long currentOperandD = unsigned long long(currentOperand) / 1000000;
+			float MixOfTextKeySqrt = currentOperandD * currentOperandD;
+			MixOfTextKeySqrt = round(MixOfTextKeySqrt);
+			MixOfTextKeySqrt = MixOfTextKeySqrt / 1000000;
+
+			float SqrtOfText = MixOfTextKeySqrt - SqKeyX;
+			float TextD = SqrtOfText * SqrtOfText;
+			TextD = round(TextD);
+
+			unsigned char TextN = (unsigned char)TextD;
+			vec.push_back(TextN);
+		}
+
+		return vec;
+	}
+
 }
--- src/NES/NES.h
+++ src/NES/NES.h
@@ -9,14 +9,23 @@
 
 namespace NES
 {
-	std::string TEXT_NESA0(std::string text, std::string key); // TA0
-	std::string TEXT_NESA1(std::string text, std::string key); // TA1
-																  
-	std::string TEXT_NESB0(std::string text, std::string key); // TB0
-	std::string TEXT_NESB1(std::string text, std::string key); // TB1
-																  
-	std::string TEXT_NESC0(std::string text, std::string key); // TC0
-	std::string TEXT_NESC1(std::string text, std::string key); // TC1
+	std::string NESA0(std::string text, std::string key); // TA0
+	std::string NESA1(std::string text, std::string key); // TA1
+
+	std::string NESB0(std::string text, std::string key); // TB0
+	std::string NESB1(std::string text, std::string key); // TB1
+
+	std::string NESC0(std::string text, std::string key); // TC0
+	std::string NESC1(std::string text, std::string key); // TC1
+
+	std::vector<BYTE> NESA0(std::vector<BYTE> data, std::string key); // DA0
+	std::vector<BYTE> NESA1(std::vector<BYTE> data, std::string key); // DA1
+																	  	 
+	std::vector<BYTE> NESB0(std::vector<BYTE> data, std::string key); // DB0
+	std::vector<BYTE> NESB1(std::vector<BYTE> data, std::string key); // DB1
+																	  	 
+	std::vector<BYTE> NESC0(std::vector<BYTE> data, std::string key); // DC0
+	std::vector<BYTE> NESC1(std::vector<BYTE> data, std::string key); // DC1
 }
 
 #endif // !_N_NES_H_
