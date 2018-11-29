1593c3412c8641868e644a631a431ea09764f194
Nirex0 <Nirex.0@gmail.com>
2018-11-29 12:43:59 +0330

    Update: Example is now an infinite loop

--- src/NES/Example.cpp
+++ src/NES/Example.cpp
@@ -19,139 +19,131 @@
 
 FILL main(NINT argc, NStrArray argv) -> NINT
 {
-	UNREFERENCED_PARAMETER(argc);
-	UNREFERENCED_PARAMETER(argv);
-	
+	while (true)
+	{
+		UNREFERENCED_PARAMETER(argc);
+		UNREFERENCED_PARAMETER(argv);
 
-	std::string szText;
-	std::string szKey;
-	std::string szEncrypted;
-	std::string szEncrypted01;
-	std::string szEncrypted02;
-	std::string szEncrypted03;
+		std::string szText;
+		std::string szKey;
+		std::string szEncrypted;
+		std::string szEncrypted01;
+		std::string szEncrypted02;
+		std::string szEncrypted03;
 
-	std::string szDecrypted;
-	std::string szDecrypted01;
-	std::string szDecrypted02;
-	std::string szDecrypted03;
+		std::string szDecrypted;
+		std::string szDecrypted01;
+		std::string szDecrypted02;
+		std::string szDecrypted03;
 
-	//COUT << "Text: ";
-	//LINE(CIN, szText);
+		COUT << "Text: ";
+		LINE(CIN, szText);
 
-	//COUT << "Key: ";
-	//LINE(CIN, szKey);
-	
-	szText = "The Secret Message!";
-	szKey = "The Master Key is: \"Master Secret Key!!!!XXXXXXXXXXXX\"";
+		COUT << "Key: ";
+		LINE(CIN, szKey);
 
-	COUT << ENDL;
+		COUT << ENDL;
 
-	szEncrypted = NES::NESA0(szText, szKey);
-	Sleep(10);
-	szEncrypted01 = NES::NESA0(szText, szKey);
-	Sleep(10);
-	szEncrypted02 = NES::NESA0(szText, szKey);
-	Sleep(10);
-	szEncrypted03 = NES::NESA0(szText, szKey);
-	szDecrypted = NES::NESA1(szEncrypted, szKey);
-	szDecrypted01 = NES::NESA1(szEncrypted01, szKey);
-	szDecrypted02 = NES::NESA1(szEncrypted02, szKey);
-	szDecrypted03 = NES::NESA1(szEncrypted03, szKey);
+		szEncrypted = NES::NESA0(szText, szKey);
+		Sleep(10);
+		szEncrypted01 = NES::NESA0(szText, szKey);
+		Sleep(10);
+		szEncrypted02 = NES::NESA0(szText, szKey);
+		Sleep(10);
+		szEncrypted03 = NES::NESA0(szText, szKey);
+		szDecrypted = NES::NESA1(szEncrypted, szKey);
+		szDecrypted01 = NES::NESA1(szEncrypted01, szKey);
+		szDecrypted02 = NES::NESA1(szEncrypted02, szKey);
+		szDecrypted03 = NES::NESA1(szEncrypted03, szKey);
 
-	COUT << "Encrypted(A_00): ";
-	COUT << szEncrypted << ENDL;
-	COUT << "Encrypted(A_01): ";
-	COUT << szEncrypted01 << ENDL;
-	COUT << "Encrypted(A_02): ";
-	COUT << szEncrypted02 << ENDL;
-	COUT << "Encrypted(A_03): ";
-	COUT << szEncrypted03 << ENDL;
+		COUT << "Encrypted(A_00): ";
+		COUT << szEncrypted << ENDL;
+		COUT << "Encrypted(A_01): ";
+		COUT << szEncrypted01 << ENDL;
+		COUT << "Encrypted(A_02): ";
+		COUT << szEncrypted02 << ENDL;
+		COUT << "Encrypted(A_03): ";
+		COUT << szEncrypted03 << ENDL;
 
-	COUT << ENDL;
+		COUT << ENDL;
 
-	COUT << "Decrypted(A_00): ";
-	COUT << szDecrypted << ENDL;
-	COUT << "Decrypted(A_01): ";
-	COUT << szDecrypted01 << ENDL;
-	COUT << "Decrypted(A_02): ";
-	COUT << szDecrypted02 << ENDL;
-	COUT << "Decrypted(A_03): ";
-	COUT << szDecrypted03 << ENDL;
-	COUT << ENDL;
+		COUT << "Decrypted(A_00): ";
+		COUT << szDecrypted << ENDL;
+		COUT << "Decrypted(A_01): ";
+		COUT << szDecrypted01 << ENDL;
+		COUT << "Decrypted(A_02): ";
+		COUT << szDecrypted02 << ENDL;
+		COUT << "Decrypted(A_03): ";
+		COUT << szDecrypted03 << ENDL;
+		COUT << ENDL;
 
-
-	szEncrypted = NES::NESB0(szText, szKey);
-	Sleep(10);
-	szEncrypted01 = NES::NESB0(szText, szKey);
-	Sleep(10);
-	szEncrypted02 = NES::NESB0(szText, szKey);
-	Sleep(10);
-	szEncrypted03 = NES::NESB0(szText, szKey);
-	szDecrypted = NES::NESB1(szEncrypted, szKey);
-	szDecrypted01 = NES::NESB1(szEncrypted01, szKey);
-	szDecrypted02 = NES::NESB1(szEncrypted02, szKey);
-	szDecrypted03 = NES::NESB1(szEncrypted03, szKey);
+		szEncrypted = NES::NESB0(szText, szKey);
+		Sleep(10);
+		szEncrypted01 = NES::NESB0(szText, szKey);
+		Sleep(10);
+		szEncrypted02 = NES::NESB0(szText, szKey);
+		Sleep(10);
+		szEncrypted03 = NES::NESB0(szText, szKey);
+		szDecrypted = NES::NESB1(szEncrypted, szKey);
+		szDecrypted01 = NES::NESB1(szEncrypted01, szKey);
+		szDecrypted02 = NES::NESB1(szEncrypted02, szKey);
+		szDecrypted03 = NES::NESB1(szEncrypted03, szKey);
 
 
-	COUT << "Encrypted(B_00): ";
-	COUT << szEncrypted << ENDL;
-	COUT << "Encrypted(B_01): ";
-	COUT << szEncrypted01 << ENDL;
-	COUT << "Encrypted(B_02): ";
-	COUT << szEncrypted02 << ENDL;
-	COUT << "Encrypted(B_03): ";
-	COUT << szEncrypted03 << ENDL;
+		COUT << "Encrypted(B_00): ";
+		COUT << szEncrypted << ENDL;
+		COUT << "Encrypted(B_01): ";
+		COUT << szEncrypted01 << ENDL;
+		COUT << "Encrypted(B_02): ";
+		COUT << szEncrypted02 << ENDL;
+		COUT << "Encrypted(B_03): ";
+		COUT << szEncrypted03 << ENDL;
 
-	COUT << ENDL;
+		COUT << ENDL;
 
-	COUT << "Decrypted(B_00): ";
-	COUT << szDecrypted << ENDL;
-	COUT << "Decrypted(B_01): ";
-	COUT << szDecrypted01 << ENDL;
-	COUT << "Decrypted(B_02): ";
-	COUT << szDecrypted02 << ENDL;
-	COUT << "Decrypted(B_03): ";
-	COUT << szDecrypted03 << ENDL;
-	COUT << ENDL;
+		COUT << "Decrypted(B_00): ";
+		COUT << szDecrypted << ENDL;
+		COUT << "Decrypted(B_01): ";
+		COUT << szDecrypted01 << ENDL;
+		COUT << "Decrypted(B_02): ";
+		COUT << szDecrypted02 << ENDL;
+		COUT << "Decrypted(B_03): ";
+		COUT << szDecrypted03 << ENDL;
+		COUT << ENDL;
 
+		szEncrypted = NES::NESC0(szText, szKey);
+		Sleep(10);
+		szEncrypted01 = NES::NESC0(szText, szKey);
+		Sleep(10);
+		szEncrypted02 = NES::NESC0(szText, szKey);
+		Sleep(10);
+		szEncrypted03 = NES::NESC0(szText, szKey);
+		szDecrypted = NES::NESC1(szEncrypted, szKey);
+		szDecrypted01 = NES::NESC1(szEncrypted01, szKey);
+		szDecrypted02 = NES::NESC1(szEncrypted02, szKey);
+		szDecrypted03 = NES::NESC1(szEncrypted03, szKey);
 
-	szEncrypted = NES::NESC0(szText, szKey);
-	Sleep(10);
-	szEncrypted01 = NES::NESC0(szText, szKey);
-	Sleep(10);
-	szEncrypted02 = NES::NESC0(szText, szKey);
-	Sleep(10);
-	szEncrypted03 = NES::NESC0(szText, szKey);
-	szDecrypted = NES::NESC1(szEncrypted, szKey);
-	szDecrypted01 = NES::NESC1(szEncrypted01, szKey);
-	szDecrypted02 = NES::NESC1(szEncrypted02, szKey);
-	szDecrypted03 = NES::NESC1(szEncrypted03, szKey);
+		COUT << "Encrypted(C_00): ";
+		COUT << szEncrypted << ENDL;
+		COUT << "Encrypted(C_01): ";
+		COUT << szEncrypted01 << ENDL;
+		COUT << "Encrypted(C_02): ";
+		COUT << szEncrypted02 << ENDL;
+		COUT << "Encrypted(C_03): ";
+		COUT << szEncrypted03 << ENDL;
 
-	COUT << "Encrypted(C_00): ";
-	COUT << szEncrypted << ENDL;
-	COUT << "Encrypted(C_01): ";
-	COUT << szEncrypted01 << ENDL;
-	COUT << "Encrypted(C_02): ";
-	COUT << szEncrypted02 << ENDL;
-	COUT << "Encrypted(C_03): ";
-	COUT << szEncrypted03 << ENDL;
+		COUT << ENDL;
 
-	COUT << ENDL;
-
-	COUT << "Decrypted(C_00): ";
-	COUT << szDecrypted << ENDL;
-	COUT << "Decrypted(C_01): ";
-	COUT << szDecrypted01 << ENDL;
-	COUT << "Decrypted(C_02): ";
-	COUT << szDecrypted02 << ENDL;
-	COUT << "Decrypted(C_03): ";
-	COUT << szDecrypted03 << ENDL;
-	COUT << ENDL;
-
-
-	int x;
-	CIN >> x;
-
-
+		COUT << "Decrypted(C_00): ";
+		COUT << szDecrypted << ENDL;
+		COUT << "Decrypted(C_01): ";
+		COUT << szDecrypted01 << ENDL;
+		COUT << "Decrypted(C_02): ";
+		COUT << szDecrypted02 << ENDL;
+		COUT << "Decrypted(C_03): ";
+		COUT << szDecrypted03 << ENDL;
+		COUT << ENDL;
+		COUT << ENDL;
+	}
 	return S_OK;
 }
