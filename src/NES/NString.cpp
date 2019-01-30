6938ffc79c62c7f613e1980c2733193c0d77e153
Nirex0 <Nirex.0@gmail.com>
2019-01-30 14:12:38 +0330

    Update: NString is now as flexible as NStringUtils of NLib

--- src/NES/NString.cpp
+++ src/NES/NString.cpp
@@ -1,4 +1,4 @@
-//© 2018 NIREX ALL RIGHTS RESERVED
+//© 2019 NIREX ALL RIGHTS RESERVED
 
 #include "NString.h"
 
@@ -30,7 +30,7 @@ namespace NString
 		return retc;
 	}
 
-	char* Proper(char* temp)
+	char* ProperizeWord(char* temp)
 	{
 		char* retc = temp;
 
@@ -42,6 +42,68 @@ namespace NString
 		}
 		retc[0] = toupper(retc[0]);
 		return retc;
+	}
+
+	char* SubString(char* str, int s_size, int from, int to)
+	{
+		if (from < 0 || to >= s_size || to < from || from >= s_size || from > to)
+		{
+			return nullptr;
+		}
+
+		std::string tmp = str;
+		return (char*)tmp.substr(from, to - from).c_str();
+	}
+
+	wchar_t* ToUpper(wchar_t* temp)
+	{
+		wchar_t* retc = temp;
+
+		int i = 0;
+		while (retc[i])
+		{
+			retc[i] = toupper(retc[i]);
+			i++;
+		}
+		return retc;
+	}
+
+	wchar_t* ToLower(wchar_t* temp)
+	{
+		wchar_t* retc = temp;
+
+		int i = 0;
+		while (retc[i])
+		{
+			retc[i] = tolower(retc[i]);
+			i++;
+		}
+		return retc;
+	}
+
+	wchar_t* ProperizeWord(wchar_t* temp)
+	{
+		wchar_t* retc = temp;
+
+		int i = 0;
+		while (retc[i])
+		{
+			retc[i] = tolower(retc[i]);
+			i++;
+		}
+		retc[0] = toupper(retc[0]);
+		return retc;
+	}
+
+	wchar_t* SubString(wchar_t* str, int s_size, int from, int to)
+	{
+		if (from < 0 || to >= s_size || to < from || from >= s_size || from > to)
+		{
+			return nullptr;
+		}
+
+		std::wstring tmp = str;
+		return (wchar_t*)tmp.substr(from, to - from).c_str();
 	}
 
 	std::string ToUpper(std::string temp)
@@ -66,7 +128,7 @@ namespace NString
 		return temp;
 	}
 
-	std::string Proper(std::string temp)
+	std::string ProperizeWord(std::string temp)
 	{
 		int i = 0;
 		while (temp[i])
@@ -76,6 +138,62 @@ namespace NString
 		}
 		temp[0] = toupper(temp[0]);
 		return temp;
+	}
+
+	std::string SubString(std::string str, int from, int to)
+	{
+		if (from < 0 || to >= (int)str.length() || to < from || from >= (int)str.length() || from > to)
+		{
+			return nullptr;
+		}
+
+		std::string tmp = str;
+		return tmp.substr(from, to - from);
+	}
+
+	std::wstring ToUpper(std::wstring temp)
+	{
+		int i = 0;
+		while (temp[i])
+		{
+			temp[i] = toupper(temp[i]);
+			i++;
+		}
+		return temp;
+	}
+
+	std::wstring ToLower(std::wstring temp)
+	{
+		int i = 0;
+		while (temp[i])
+		{
+			temp[i] = tolower(temp[i]);
+			i++;
+		}
+		return temp;
+	}
+
+	std::wstring ProperizeWord(std::wstring temp)
+	{
+		int i = 0;
+		while (temp[i])
+		{
+			temp[i] = tolower(temp[i]);
+			i++;
+		}
+		temp[0] = toupper(temp[0]);
+		return temp;
+	}
+
+	std::wstring SubString(std::wstring str, int from, int to)
+	{
+		if (from < 0 || to >= (int)str.length() || to < from || from >= (int)str.length() || from > to)
+		{
+			return nullptr;
+		}
+
+		std::wstring tmp = str;
+		return tmp.substr(from, to - from);
 	}
 
 	std::vector<std::string> Split(std::string in_s, const std::string& delim)
@@ -95,7 +213,222 @@ namespace NString
 		return retvec;
 	}
 
-	std::string HexString(const std::string& in_s)
+	std::vector<std::wstring> Split(std::wstring in_s, const std::wstring & delim)
+	{
+		std::vector <std::wstring> retvec;
+		size_t pos = 0;
+		std::wstring token;
+
+		while ((pos = in_s.find(delim)) != std::wstring::npos)
+		{
+			token = in_s.substr(0, pos);
+			retvec.push_back(token);
+			in_s.erase(0, pos + delim.length());
+		}
+
+		retvec.push_back(in_s);
+		return retvec;
+	}
+
+	std::vector<std::string> SplitNoEmpty(std::string in_s, const std::string & delim)
+	{
+		std::vector <std::string> retvec;
+		size_t pos = 0;
+		std::string token;
+
+		while ((pos = in_s.find(delim)) != std::string::npos)
+		{
+			token = in_s.substr(0, pos);
+			retvec.push_back(token);
+			in_s.erase(0, pos + delim.length());
+		}
+
+		retvec.push_back(in_s);
+
+		std::vector<std::string> propervec;
+		for (const std::string& val : retvec)
+		{
+			if (val != "")
+			{
+				propervec.push_back(val);
+			}
+		}
+		return propervec;
+	}
+
+	std::vector<std::wstring> SplitNoEmpty(std::wstring in_s, const std::wstring & delim)
+	{
+		std::vector <std::wstring> retvec;
+		size_t pos = 0;
+		std::wstring token;
+
+		while ((pos = in_s.find(delim)) != std::wstring::npos)
+		{
+			token = in_s.substr(0, pos);
+			retvec.push_back(token);
+			in_s.erase(0, pos + delim.length());
+		}
+
+		retvec.push_back(in_s);
+
+		std::vector<std::wstring> propervec;
+		for (const std::wstring& val : retvec)
+		{
+			if (val != L"")
+			{
+				propervec.push_back(val);
+			}
+		}
+		return propervec;
+	}
+
+	std::wstring ToWide(std::string in)
+	{
+		std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>> converter;
+		return converter.from_bytes(in);
+	}
+
+	std::string ToNarrow(std::wstring in)
+	{
+		std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>> converter;
+		return converter.to_bytes(in);
+	}
+
+	std::vector<std::string> ToVector(std::string temp)
+	{
+		return SplitNoEmpty(temp, "\n");
+	}
+
+	std::string FromVector(std::vector<std::string> temp)
+	{
+		std::string retS;
+		for (const auto& p : temp)
+		{
+			retS += p;
+			retS += "\n";
+		}
+		return retS;
+	}
+
+	std::vector<std::wstring> ToVector(std::wstring temp)
+	{
+		return SplitNoEmpty(temp, L"\n");
+	}
+
+	std::wstring FromVector(std::vector<std::wstring> temp)
+	{
+		std::wstring retS;
+		for (const auto& p : temp)
+		{
+			retS += p;
+			retS += L"\n";
+		}
+		return retS;
+	}
+
+	std::string FullTrim(std::string temp)
+	{
+		return TrimBegin(TrimEnd(temp));
+	}
+
+	std::wstring FullTrim(std::wstring temp)
+	{
+		return TrimBegin(TrimEnd(temp));
+	}
+
+	std::string TrimBegin(std::string temp)
+	{
+		temp.erase(temp.begin(), std::find_if(temp.begin(), temp.end(), [](int ch) {
+			return !std::isspace(ch);
+		}));
+		return temp;
+	}
+
+	std::wstring TrimBegin(std::wstring temp)
+	{
+		temp.erase(temp.begin(), std::find_if(temp.begin(), temp.end(), [](int ch) {
+			return !std::isspace(ch);
+		}));
+		return temp;
+	}
+
+	std::string TrimEnd(std::string temp)
+	{
+		temp.erase(std::find_if(temp.rbegin(), temp.rend(), [](int ch) {
+			return !std::isspace(ch);
+		}).base(), temp.end());
+		return temp;
+	}
+
+	std::wstring TrimEnd(std::wstring temp)
+	{
+		temp.erase(std::find_if(temp.rbegin(), temp.rend(), [](int ch) {
+			return !std::isspace(ch);
+		}).base(), temp.end());
+		return temp;
+	}
+
+	std::string Replace(std::string str, std::string original, std::string rps)
+	{
+		str.replace(str.find(original), original.length(), rps);
+		return str;
+	}
+
+	std::wstring Replace(std::wstring wstr, std::wstring original, std::wstring rps)
+	{
+		wstr.replace(wstr.find(original), original.length(), rps);
+		return wstr;
+	}
+
+	std::string RegexReplace(std::string str, std::string original, std::string rps)
+	{
+		return std::regex_replace(str, std::regex(original), rps);
+	}
+
+	std::wstring RegexReplace(std::wstring wstr, std::wstring original, std::wstring rps)
+	{
+		return ToWide(std::regex_replace(ToNarrow(wstr), std::regex(ToNarrow(original)), ToNarrow(rps)));
+	}
+
+	bool Validate(const std::string& str, const std::set<std::string>& set)
+	{
+		for (const auto& s : set)
+		{
+			if (s == str) return true;
+		}
+		return false;
+	}
+
+	bool Validate(const std::wstring& str, const std::set<std::wstring>& set)
+	{
+		for (const auto& s : set)
+		{
+			if (s == str) return true;
+		}
+		return false;
+	}
+
+	long Contains(const std::string& str, const::std::string& substr)
+	{
+		size_t index = str.find(substr);
+		if (index != std::string::npos)
+		{
+			return index;
+		}
+		return -1;
+	}
+
+	long Contains(const std::wstring& str, const::std::wstring& substr)
+	{
+		size_t index = str.find(substr);
+		if (index != std::string::npos)
+		{
+			return index;
+		}
+		return -1;
+	}
+
+	std::string ToHex(const std::string & in_s)
 	{
 		static const char* const lut = "0123456789ABCDEF";
 		size_t len = in_s.length();
@@ -111,21 +444,21 @@ namespace NString
 		return output;
 	}
 
-	std::string NormalString(const std::string& in_s)
+	std::string FromHex(const std::string & in_hex)
 	{
 		static const char* const lut = "0123456789ABCDEF";
-		size_t len = in_s.length();
+		size_t len = in_hex.length();
 		if (len & 1) throw std::invalid_argument("odd length");
 
 		std::string output;
 		output.reserve(len / 2);
 		for (size_t i = 0; i + 1 < len; i += 2)
 		{
-			char a = in_s[i];
+			char a = in_hex[i];
 			const char* p = std::lower_bound(lut, lut + 16, a);
 			if (*p != a) throw std::invalid_argument("not a hex digit");
 
-			char b = in_s[i + 1];
+			char b = in_hex[i + 1];
 			const char* q = std::lower_bound(lut, lut + 16, b);
 			if (*q != b) throw std::invalid_argument("not a hex digit");
 
@@ -133,7 +466,8 @@ namespace NString
 		}
 		return output;
 	}
-	std::string Replace(std::string in_s ,unsigned int index_a, unsigned int index_b)
+
+	std::string Replace(std::string in_s, unsigned int index_a, unsigned int index_b)
 	{
 		if (index_a >= in_s.length() || index_b >= in_s.length())
 		{
--- src/NES/NString.h
+++ src/NES/NString.h
@@ -1,4 +1,4 @@
-//© 2018 NIREX ALL RIGHTS RESERVED
+//© 2019 NIREX ALL RIGHTS RESERVED
 
 #ifndef _N_STRING_H_
 #define _N_STRING_H_
@@ -6,25 +6,76 @@
 #include "Precompiled.h"
 #include <string>
 #include <vector>
+#include <set>
 #include <iomanip>
 #include <sstream>
 #include <algorithm>
 #include <stdexcept>
+#include <cctype>
+#include <locale>
+#include <codecvt>
+#include <regex>
 
 namespace NString
 {
-	char* ToUpper(char* temp);
-	char* ToLower(char* temp);
-	char* Proper(char* temp);
+	static char* ToUpper(char* temp);
+	static char* ToLower(char* temp);
+	static char* ProperizeWord(char* temp);
+	static char* SubString(char* str, int size, int from, int to);
 
-	std::string ToUpper(std::string temp);
-	std::string ToLower(std::string temp);
-	std::string Proper(std::string temp);
+	static wchar_t* ToUpper(wchar_t* temp);
+	static wchar_t* ToLower(wchar_t* temp);
+	static wchar_t* ProperizeWord(wchar_t* temp);
+	static wchar_t* SubString(wchar_t* str, int s_size, int from, int to);
 
-	std::vector<std::string> Split(std::string in_s, const std::string& delim);
+	static std::string ToUpper(std::string temp);
+	static std::string ToLower(std::string temp);
+	static std::string ProperizeWord(std::string temp);
+	static std::string SubString(std::string str, int from, int to);
 
-	std::string HexString(const std::string& in_s);
-	std::string NormalString(const std::string& in_hex);
+	static std::wstring ToUpper(std::wstring temp);
+	static std::wstring ToLower(std::wstring temp);
+	static std::wstring ProperizeWord(std::wstring temp);
+	static std::wstring SubString(std::wstring str, int from, int to);
+
+	static std::vector<std::string> Split(std::string in_s, const std::string& delim);
+	static std::vector<std::wstring> Split(std::wstring in_s, const std::wstring& delim);
+
+	static std::vector<std::string> SplitNoEmpty(std::string in_s, const std::string& delim);
+	static std::vector<std::wstring> SplitNoEmpty(std::wstring in_s, const std::wstring& delim);
+
+	static std::wstring ToWide(std::string in);
+	static std::string ToNarrow(std::wstring in);
+
+	static std::vector<std::string> ToVector(std::string temp);
+	static std::string FromVector(std::vector<std::string> temp);
+
+	static std::vector<std::wstring> ToVector(std::wstring temp);
+	static std::wstring FromVector(std::vector<std::wstring> temp);
+
+	static std::string FullTrim(std::string temp);
+	static std::wstring FullTrim(std::wstring temp);
+
+	static std::string TrimBegin(std::string temp);
+	static std::wstring TrimBegin(std::wstring temp);
+
+	static std::string TrimEnd(std::string temp);
+	static std::wstring TrimEnd(std::wstring temp);
+
+	static std::string Replace(std::string str, std::string original, std::string rps);
+	static std::wstring Replace(std::wstring wstr, std::wstring original, std::wstring rps);
+
+	static std::string RegexReplace(std::string str, std::string original, std::string rps);
+	static std::wstring RegexReplace(std::wstring wstr, std::wstring original, std::wstring rps);
+
+	static bool Validate(const std::string& str, const std::set<std::string>& set);
+	static bool Validate(const std::wstring& str, const std::set<std::wstring>& set);
+
+	static long Contains(const std::string& str, const::std::string& substr);
+	static long Contains(const std::wstring& str, const::std::wstring& substr);
+
+	std::string ToHex(const std::string& in_s);
+	std::string FromHex(const std::string& in_hex);
 
 	std::string Replace(std::string in_s, unsigned int index_a, unsigned int index_b);
 }
