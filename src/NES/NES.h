70d5d17a858cd42c81f27a1f79f47f6f5e905cd0
Nirex0 <Nirex.0@gmail.com>
2019-03-04 15:23:45 +0330

    Update: NES.h function names

--- src/NES/NES.h
+++ src/NES/NES.h
@@ -7,35 +7,35 @@
 #include "NString.h"
 #include <vector>
 
-namespace BMP // Basic Math Pipelines
+namespace BP // Basic Pipelines
 {
-	std::string BM_A0(std::string text, std::string key); // TA0
-	std::string BM_A1(std::string text, std::string key); // TA1
+	std::string EncryptA(std::string text, std::string key);
+	std::string DecryptA(std::string text, std::string key);
 
-	std::string BM_B0(std::string text, std::string key); // TB0
-	std::string BM_B1(std::string text, std::string key); // TB1
+	std::string EncryptB(std::string text, std::string key);
+	std::string DecryptB(std::string text, std::string key);
 
-	std::string BM_C0(std::string text, std::string key); // TC0
-	std::string BM_C1(std::string text, std::string key); // TC1
+	std::string EncryptC(std::string text, std::string key);
+	std::string DecryptC(std::string text, std::string key);
 
-	std::vector<BYTE> BM_A0(std::vector<BYTE> data, std::string key); // DA0
-	std::vector<BYTE> BM_A1(std::vector<BYTE> data, std::string key); // DA1
-																	  	 
-	std::vector<BYTE> BM_B0(std::vector<BYTE> data, std::string key); // DB0
-	std::vector<BYTE> BM_B1(std::vector<BYTE> data, std::string key); // DB1
-																	  	 
-	std::vector<BYTE> BM_C0(std::vector<BYTE> data, std::string key); // DC0
-	std::vector<BYTE> BM_C1(std::vector<BYTE> data, std::string key); // DC1
+	std::vector<BYTE> EncryptA(const std::vector<BYTE> &data, std::string key);
+	std::vector<BYTE> DecryptA(std::vector<BYTE> data, std::string key);
+																	  	
+	std::vector<BYTE> EncryptB(std::vector<BYTE> data, std::string key);
+	std::vector<BYTE> DecryptB(std::vector<BYTE> data, std::string key);
+																	  	
+	std::vector<BYTE> EncryptC(std::vector<BYTE> data, std::string key);
+	std::vector<BYTE> DecryptC(std::vector<BYTE> data, std::string key);
 }
 
-namespace AMP // Advanced Math Pipelines
+namespace AP // Advanced Pipelines
 {
 }
 
-namespace MOP // Mixing Operation Pipelines
+namespace MP // Mixing Pipelines
 {
-	std::string MO_A0(std::string in_s);
-	std::string MO_A1(std::string in_s);
+	std::string MixA(std::string in_s);
+	std::string MegaMixA(std::string in_s);
 }
 
 #endif // !_N_NES_H_
